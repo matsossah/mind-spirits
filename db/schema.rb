10f4e15988859a86a3ffb9008f6c9a46845323a6
Guillaume Caulier <cauliergui@gmail.com>
2016-03-08 13:32:27 +0100

    add a name to user (migration), signin has the same style css as Login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307170349) do
+ActiveRecord::Schema.define(version: 20160308122153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20160307170349) do
     t.datetime "updated_at",                          null: false
     t.string   "address"
     t.string   "phone_number"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
