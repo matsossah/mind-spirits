446863d76d7fc7f5d0e89d6f21691abac9124ce9
Mateo Sossah <msossah@batling.fr>
2016-03-17 17:03:55 +0100

    rediscloud

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -12,13 +12,3 @@ if url
   end
   $redis = Redis.new(:url => url)
 end
-Add a second line to your Procfile
-# Procfile
-web: bundle exec puma -C config/puma.rb
-worker: bundle exec sidekiq -C config/sidekiq.yml
-HEROKU CRON JOBS
-
-Use the Heroku Scheduler
-
-HAPPY ASYNCHRONIZING!
-
