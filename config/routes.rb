8a5a6ad3039e5509464533051741b2bed3d9bce7
Mateo Sossah <msossah@batling.fr>
2016-03-17 12:06:34 +0100

    installed reddis and sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
  Rails.application.routes.draw do
+  require "sidekiq/web"
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root :to => "cocktails#index"
+
+  authenticate :user, lambda { |u| u.admin } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
