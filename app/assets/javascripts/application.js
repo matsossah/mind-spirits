1496367567e9e60d20f035e15685ae9c27253438
Guillaume Caulier <cauliergui@gmail.com>
2016-03-18 12:49:16 +0100

    gmpas4rails en commentaire parce qu'il fait tout péter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -71,13 +71,13 @@ function distance(lat1, lon1, lat2, lon2) {
   return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
 }
 
-function define_markers(professionals) //sends back an array of hashes [{:lat=>50.635793, :lng=>3.045787}]
-  markers = Gmaps4rails.build_markers(professionals) do |professional, marker|
-    marker.lat professional.latitude
-    marker.lng professional.longitude
-  end
+// function define_markers(professionals) //sends back an array of hashes [{:lat=>50.635793, :lng=>3.045787}]
+//   markers = Gmaps4rails.build_markers(professionals) do |professional, marker|
+//     marker.lat professional.latitude
+//     marker.lng professional.longitude
+//   end
 
-}
+// }
 
 $('#user_input_autocomplete_address').on('blur', function() {
   setTimeout(function() {
