c6d4f1b72cbd018458619c889827baa288596ef9
Mateo Sossah <msossah@batling.fr>
2016-03-21 16:05:14 +0100

    prepare

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -114,32 +114,50 @@ $('#user_input_autocomplete_address').on('blur', function(event) {
           handler.fitMapToBounds();
         });
 
-        for (i = 0; i < pros_to_json.length; i++) {
+        markers.forEach( function (marker) {
           google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
               infowindow.setContent(valid_pros[i].business_address);
               infowindow.open(map, marker);
             }
           })(marker, i));
-        }
+        });
       });
     });
   }, 500);
 });
 
+// var map;
+// function initialize() {
+//   var mapOptions = {
+//     zoom: 8,
+//     center: {lat: -34.397, lng: 150.644}
+//   };
+//   map = new google.maps.Map(document.getElementById('map'),
+//       mapOptions);
 
-    //
-    //var valid_pros = define_markers(pros_in_range);
-    //
-    //valid_pros.forEach(function(pro) {
-    //    handler = Gmaps.build('Google');
-    //    handler.buildMap({ internal: { id: 'map' } }, function(){
-    //      markers = handler.addMarkers(<%= raw valid_pros.to_json %>);
-    //      handler.bounds.extendWith(markers);
-    //      handler.fitMapToBounds();
-    //    });
-    //}
+//   var marker = new google.maps.Marker({
+//     // The below line is equivalent to writing:
+//     // position: new google.maps.LatLng(-34.397, 150.644)
+//     position: {lat: -34.397, lng: 150.644},
+//     map: map
+//   });
 
+//   // You can use a LatLng literal in place of a google.maps.LatLng object when
+//   // creating the Marker object. Once the Marker object is instantiated, its
+//   // position will be available as a google.maps.LatLng object. In this case,
+//   // we retrieve the marker's position using the
+//   // google.maps.LatLng.getPosition() method.
+//   var infowindow = new google.maps.InfoWindow({
+//     content: '<p>Marker Location:' + marker.getPosition() + '</p>'
+//   });
+
+//   google.maps.event.addListener(marker, 'click', function() {
+//     infowindow.open(map, marker);
+//   });
+// }
+
+// google.maps.event.addDomListener(window, 'load', initialize);
 
 function fillForm(element) {
   var professional_id = $(element).attr('data-id');
