e1bceb23e29b78cc0eed99a44d888596f9269432
Mateo Sossah <msossah@batling.fr>
2016-03-22 18:00:34 +0100

    event confirmation ajaxified motherfuckaaaaaaa

--- app/controllers/professionals/events_controller.rb
+++ app/controllers/professionals/events_controller.rb
@@ -7,21 +7,18 @@ module Professionals
     def confirm
       @event = Event.find(params[:event_id])
       @event.is_confirmed = true
-      @event.save
 
       if @event.save
         respond_to do |format|
-          format.html { redirect_to user_professional_events(current_user) }
+          # format.html { redirect_to user_professional_events(current_user) }
           format.js  # <-- will render `app/views/reviews/create.js.erb`
         end
       else
         respond_to do |format|
-          format.html { render 'professionals/events/index' }
+          # format.html { render 'professionals/events/index' }
           format.js  # <-- idem
         end
       end
-
-      redirect_to :back
     end
   end
 end
