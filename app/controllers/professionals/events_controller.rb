9538bd3306de1588bf1f754f5c90fc258e478260
Mateo Sossah <msossah@batling.fr>
2016-03-22 17:29:51 +0100

    ajax steps

--- app/controllers/professionals/events_controller.rb
+++ app/controllers/professionals/events_controller.rb
@@ -5,9 +5,21 @@ module Professionals
     end
 
     def confirm
-      event = Event.find(params[:event_id])
-      event.is_confirmed = true
-      event.save
+      @event = Event.find(params[:event_id])
+      @event.is_confirmed = true
+      @event.save
+
+      if @event.save
+        respond_to do |format|
+          format.html { redirect_to user_professional_events(current_user) }
+          format.js  # <-- will render `app/views/reviews/create.js.erb`
+        end
+      else
+        respond_to do |format|
+          format.html { render 'professionals/events/index' }
+          format.js  # <-- idem
+        end
+      end
 
       redirect_to :back
     end
